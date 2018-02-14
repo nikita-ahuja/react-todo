3b2f1c11cd9d0b7053ee580a93ca2e5c547ad2b1
Nikita Ahuja <nahuja038@gmail.com>
2018-02-14 16:45:07 -0600

    form redesign with solid color - throw out bckgrnd image

--- src/components/todos-list-item.js
+++ src/components/todos-list-item.js
@@ -11,11 +11,21 @@ export default class TodosListItem extends React.Component {
 
     renderTaskSection() {
       const { task, isCompleted } = this.props;
-console.log(this.props);
+      console.log(this.props);
       const taskStyle = {
         color: isCompleted ? 'green' : 'red',
         cursor: 'pointer'
       };
+
+      if (this.state.isEditing) {
+        return (
+          <td>
+            <form onSubmit={this.onSaveClick.bind(this)}
+              <input type="text" defaultValue={task} ref="editInput" />
+            </form>
+          </td>
+        )
+      }
 
       return (
         <td style={taskStyle}
@@ -61,8 +71,15 @@ console.log(this.props);
     this.setState({ isEditing: false});
   }
 
-  onSaveClick(){
-    this.setState({ isEditing: false});
+  onSaveClick(event){
+    event.preventDefault();
+
+    const oldTask = this.props.task;
+    const newTask = this.refs.editInput.value;
+    this.props.saveTask(oldTask, newTask);
+    this.setState({ isEditing: false });
+  
+
   }
 
   onDeleteClick(){
